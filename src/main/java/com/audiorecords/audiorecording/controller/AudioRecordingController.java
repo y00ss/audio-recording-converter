e68b16e3fe95afa0fe224ded7353a4e5543158d6
y00ss <youssef.dev@pm.me>
2023-06-26 00:45:37 +0200

    fix

--- src/main/java/com/audiorecords/audiorecording/controller/AudioRecordingController.java
+++ src/main/java/com/audiorecords/audiorecording/controller/AudioRecordingController.java
@@ -20,7 +20,7 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
 @RestController
-@CrossOrigin(origins = {"*"}, maxAge = 4800, allowCredentials = "false")
+//@CrossOrigin(origins = {"*"}, maxAge = 4800, allowCredentials = "false")
 @RequestMapping("/api")
 public class AudioRecordingController {
 
